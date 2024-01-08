9fea97cbec0399ccb288cc1ad958838c1fb13b72
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2024-01-08 10:05:52 -0600

    Commit before run single_qubit_shadow

--- build/single_qubit_shadow/single_qubit_lindblad_equation.py
+++ build/single_qubit_shadow/single_qubit_lindblad_equation.py
@@ -191,8 +191,8 @@ def quadratic_function_prediction(N,K,operator_m,operator_n,list_of_shadows):
 # In[ ]:
 
 
-N = 25
-K = 20
+N = 30
+K = 10
 N_qubits = 2
 n_Shadows = N*K
 number_of_angle_divisons = 100
