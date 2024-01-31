5ccda99fd203adeb74e6dde18446c6f9d88fc82c
Sasanka Dowarah <sxd190113@login-01.localdomain>
2024-01-30 20:50:26 -0600

    Commit before run single_qubit_2d

--- build/single_qubit_2d/trotterization_H2_molecule.py
+++ build/single_qubit_2d/trotterization_H2_molecule.py
@@ -320,7 +320,7 @@ def trotter_circuit(time_step,final_time,initial_state):
     anc = QuantumRegister(1,"ancilla")
 
     # Add a classical register
-    cr = ClassicalRegister(L,"c")
+    cr = ClassicalRegister(L+1,"c")
     # Create a new quantum circuit with the classical register
     qc = QuantumCircuit(anc,qr, cr)
 
@@ -341,10 +341,11 @@ def trotter_circuit(time_step,final_time,initial_state):
     for _ in range(number_of_iterations):
         qc = qc.compose(transpiled_one_step_circuit)
 
-    """qc.measure(qr[3], cr[3])   
+    qc.measure(anc,cr[4])
+    qc.measure(qr[3], cr[3])   
     qc.measure(qr[2], cr[2])
     qc.measure(qr[1], cr[1])
-    qc.measure(qr[0], cr[0])""";
+    qc.measure(qr[0], cr[0])
 
     #print("Circuit depth = ",qc.depth())
     return qc
