e4d9956a6db2a4f99f2b8c079505c8f62940b1e3
Sasanka Dowarah <sxd190113@login-01.localdomain>
2024-02-21 12:32:09 -0600

    Commit before run single_qubit_shadow

--- build/single_qubit_shadow/expectation_value_of_pauli_strings.py
+++ build/single_qubit_shadow/expectation_value_of_pauli_strings.py
@@ -14,9 +14,6 @@ from qiskit.quantum_info import DensityMatrix
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix, kron
 
-# %%
-# working directory
-#path =("/Users/sasankadowarah/Ultimate_QM_MM/")
 
 """
 Identity and Pauli matrices .
@@ -41,8 +38,8 @@ def controlled_ry_gate_matrix(angle_theta):
 def controlled_not_gate_matrix():
         return kron(I2,PI_0) + kron(sigma_x,PI_1)
 
-gamma_in = 0.6
-gamma_out = 0.6
+gamma_in = 1.6
+gamma_out = 1.6
 
 # %% [markdown]
 # #### Hamiltonian of $H_{2}$ with Jordan Wigner transformation
@@ -53,13 +50,11 @@ def complex_converter(s):
     return complex(s.decode('utf-8'))
 
 # loads the Jordan Wigner transformed Hamiltonian
-#os.chdir(path)
 data = np.loadtxt("hydrogen_jw_hamiltonian.txt", delimiter='\t', converters={1: complex_converter}, dtype=object)
 
 # Separate the data into two arrays
 H_pauli_lst, H_pauli_coeff_lst = data.T
 
-# %%
 
 # %%
 # number of qubits
@@ -106,58 +101,7 @@ def sparse_Pauli_to_dense_matrix(sparse_Pauli_matrices_lst, Pauli_matrices_coeff
 
 full_hamiltonian = sparse_Pauli_to_dense_matrix(H_pauli_lst,H_pauli_coeff_lst);
 
-# %% [markdown]
-# #### $n=1$ and $n=2$ states of the Hamiltonian
 
-# %%
-# n = 1 basis states 0001,0010,0100,1000
-"""
-def ket_from_binary_string(binary_string):
-        bin_dict = {"0":np.matrix([1,0]), "1":np.matrix([0,1])}
-        b_0 = bin_dict[binary_string[-1]]
-        for b in range(len(binary_string)-2,-1,-1):
-                b_0 = kron(bin_dict[binary_string[b]],b_0)
-        return b_0
-
-n_1_sector_basis_states = [ket_from_binary_string("0001"),ket_from_binary_string("0010"),ket_from_binary_string("0100"),
-                           ket_from_binary_string("1000")]
-
-n_1_sector_size = 4
-H_n_1_sector = np.zeros((n_1_sector_size,n_1_sector_size),dtype=np.complex128)
-
-for m in range(len(n_1_sector_basis_states)):
-        for n in range(len(n_1_sector_basis_states)):
-                ket_m = n_1_sector_basis_states[m]
-                ket_n = n_1_sector_basis_states[n]
-                H_n_1_sector[m,n] = (ket_m *full_hamiltonian* ket_n.T).A[0,0]
-
-
-# n = 2 basis states 0011,0101,0110,1001,1010,1100
-n_2_sector_basis_states = [ket_from_binary_string("0011"),ket_from_binary_string("0101"),ket_from_binary_string("0110"),
-                           ket_from_binary_string("1001"),ket_from_binary_string("1010"),
-                           ket_from_binary_string("1100")]
-
-n_2_sector_size = 6
-H_n_2_sector = np.zeros((n_2_sector_size,n_2_sector_size),dtype=np.complex128)
-
-for m in range(len(n_2_sector_basis_states)):
-        for n in range(len(n_2_sector_basis_states)):
-                ket_m = n_2_sector_basis_states[m]
-                ket_n = n_2_sector_basis_states[n]
-                H_n_2_sector[m,n] = (ket_m *full_hamiltonian* ket_n.T).A[0,0]
-
-# %%
-n_2_eigvals, n_2_eigstates = np.linalg.eigh(H_n_2_sector)
-#n_2_eigvals, np.around(n_2_eigstates,2)
-
-# %%
-n_1_eigvals, n_1_eigstates = np.linalg.eigh(H_n_1_sector)
-#n_1_eigvals, n_1_eigstates"""
-
-# %% [markdown]
-# #### One time step circuit for trotterization
-
-# %%
 """
 
 The list of Pauli strings is 
@@ -382,10 +326,6 @@ def one_time_step_circuit(dt,barrier_status):
     qc_h2.x(qr[0])
     qc_h2.initialize([1,0],anc)
     return qc_h2
-#one_time_step_circuit(0.1,True).draw("mpl",style="iqp",scale=1)#.savefig("H2_Lindbladian_circuit_not_optimized.jpg",dpi=200)
-
-# %% [markdown]
-# #### Complete trotter circuit for time evolution
 
 # %%
 def time_evolved_density_matrix(time_step,final_time,initial_state):
@@ -682,14 +622,6 @@ I_out_term_1_term_2 = add_similar_elements((L_2_dag_H_L_2()[0],L_2_dag_H_L_2()[1
 # L_2_dag_H_L_2 - (1/2)*H_L2_dag_L2 - (1/2)*L_2_dag_L_2_H
 I_out_term_1_term_2_term_3 = add_similar_elements((I_out_term_1_term_2[0],I_out_term_1_term_2[1]),(L_2_dag_L_2_H()[0],(-1/2)*np.array(L_2_dag_L_2_H()[1])))        
 
-# %%
-"""h_intermediate_pauli_lst = hamiltonian_product_pauli(H_pauli_lst, H_pauli_coeff_lst, "Y", "left")[0]
-h_intermediate_pauli_coefficient = hamiltonian_product_pauli(H_pauli_lst, H_pauli_coeff_lst, "Y", "left")[1]
-
-h_pauli_lst = hamiltonian_product_pauli(h_intermediate_pauli_lst, h_intermediate_pauli_coefficient, "Y", "right")[0]
-h_pauli_coefficient = hamiltonian_product_pauli(h_intermediate_pauli_lst, h_intermediate_pauli_coefficient, "Y", "right")[1]
-
-h_pauli_coefficient+H_pauli_coeff_lst""";
 
 # %%
 # Unpack the pair into two lists
@@ -709,12 +641,14 @@ filtered_I_out_term_1_term_2_term_3 = ([x for x, y in zip(list1, list2) if y != 
 # #### Noise model for the simulator
 
 # %%
+
 noise_index = int(sys.argv[1])
 
-noise_factor = np.linspace(1,200,16)
+noise_factor = np.linspace(1,1000,32)
 
 T1_noise = 213.07e3/noise_factor[noise_index]
 T2_noise = 115.57e3/noise_factor[noise_index]
+
 
 T1_standard_deviation = T1_noise/4
 T2_standard_deviation = T2_noise/4
@@ -770,7 +704,7 @@ for j in range(L+1):
     for k in range(L+1):
         noise_thermal.add_quantum_error(errors_cx[j][k], "cx", [j, k])
         noise_thermal.add_quantum_error(errors_ecr[j][k], "ecr", [j, k])   
-#print(noise_thermal)    
+print(noise_thermal)    
 
 # %% [markdown]
 # #### Trotterization of the system
@@ -863,6 +797,11 @@ I_in_coefficients = filtered_I_in_term_1_term_2_term_3[1]
 I_out_pauli_list = filtered_I_out_term_1_term_2_term_3[0]
 I_out_coefficients = filtered_I_out_term_1_term_2_term_3[1]
 
+np.save("I_in_pauli_list.npy", I_in_pauli_list)
+np.save("I_in_coefficients.npy", I_in_coefficients)
+np.save("I_out_pauli_list.npy", I_out_pauli_list)
+np.save("I_out_coefficients.npy", I_out_coefficients)
+
 # %%
 number_of_shots = 8192
 r"""
@@ -875,6 +814,16 @@ Output: List of expectation values of each Pauli string in the input list
 e.g. [Expectation value("XXYY"), Expectation value("IZIZ")]
 
 """
+
+def expectation_value_of_pauli_string_from_counts(pauli_string, counts_dictionary_from_measurement):
+
+        pauli_string_expectation_value = 0.0
+        non_identity_indices = [i for i, char in enumerate(pauli_string) if char != "I"]
+        for key, value in counts_dictionary_from_measurement.items():
+                filtered_binary_string = "".join([char for i, char in enumerate(key) if i in non_identity_indices])
+                pauli_string_expectation_value += ((-1)**sum(int(num) for num in filtered_binary_string))* (value/number_of_shots)
+        return pauli_string_expectation_value
+
 def current_expectation_value(current_operator_pauli_strings, time):
 
         current_expectation_value_lst = []
@@ -885,228 +834,17 @@ def current_expectation_value(current_operator_pauli_strings, time):
                 bit_strings_and_counts = trotter_simulation_and_return_counts(pauli_string,
                                                 0.1,"0100",time,number_of_shots)
 
-                pauli_string_expectation_value = 0
 
-                # iterating over the measurement results to calculate the expectation value of the Pauli string        
-                for key, value in bit_strings_and_counts.items():
-
-                        pauli_string_expectation_value += ((-1)**sum(int(num) for num in key))* (value/number_of_shots)
+                # calculating the expectation value of the Pauli string from counts       
+                pauli_string_expectation_value = expectation_value_of_pauli_string_from_counts(pauli_string, bit_strings_and_counts)
 
                 current_expectation_value_lst.append(pauli_string_expectation_value)                                
 
         return current_expectation_value_lst
 
 # %%
-"""def X_H_X():
-        h1,p1 = hamiltonian_product_pauli(H_pauli_lst,H_pauli_coeff_lst,"X","left") # XH
-        h2, p2 = hamiltonian_product_pauli(h1,p1,"X","right") # XHX
-        return h2, p2
+time_lst = np.linspace(0.0,100,20)
 
-def Y_H_Y():
-        h1,p1 = hamiltonian_product_pauli(H_pauli_lst,H_pauli_coeff_lst,"Y","left") # YH
-        h2, p2 = hamiltonian_product_pauli(h1,p1,"Y","right") # YHY       
-        return h2, p2
-simplified_term_1_term_2 = add_similar_elements(( X_H_X()[0],np.array( X_H_X()[1])),(Y_H_Y()[0],np.array(Y_H_Y()[1])))
-
-simplified_term_1_term_2_term_3 = add_similar_elements((simplified_term_1_term_2[0],1/2*np.array(simplified_term_1_term_2[1])),(H_pauli_lst, -H_pauli_coeff_lst))
-# Unpack the pair into two lists
-list1, list2 = simplified_term_1_term_2_term_3
-
-# Use a list comprehension to filter the elements where the corresponding element in the second list is not 0 or 0j
-simplified_I_total_term_1_term_2_term_3 = ([x for x, y in zip(list1, list2) if y != 0j], [y for y in list2 if y != 0j])""";
-
-# %% [markdown]
-# #### Current vs time plot
-
-# %%
-"""time_lst = np.linspace(0.1,20,5)
-
-for time in time_lst:        
-        t = current_expectation_value(simplified_I_total_term_1_term_2_term_3[0], time)
-        os.chdir("/Users/sasankadowarah/Ultimate_QM_MM/current_data_folder")
-        np.save("t_"+str(np.around(time,2))+".npy",t)""";
-
-# %%
-"""I_tot_expectation_value_as_a_function_of_time_lst = []
-for time in time_lst:
-
-        t_load = np.load("t_"+str(np.around(time,2))+".npy",allow_pickle=True)
-        
-        I_tot_time = 0.0
-
-        for i in range(len(t_load)):
-                I_tot_time += simplified_I_total_term_1_term_2_term_3[1][i]*t_load[i]
-
-        I_tot_expectation_value_as_a_function_of_time_lst.append(I_tot_time)
-f = plt.figure()
-f.set_figwidth(10)
-f.set_figheight(6)        
-plt.plot(time_lst, gamma_in*np.array(I_tot_expectation_value_as_a_function_of_time_lst).real)
-plt.scatter(time_lst, gamma_in*np.array(I_tot_expectation_value_as_a_function_of_time_lst).real)
-plt.savefig("I_total.jpg",dpi=300)
-plt.show()""";
-
-# %%
-time_lst = np.linspace(0.0,10,5)
-
-for time in time_lst:        
+for time in time_lst: 
         t = current_expectation_value(I_in_pauli_list, time)
         np.save("t_"+str(np.around(time,2))+".npy",t)
-
-# %%
-I_in_expectation_value_as_a_function_of_time_lst = []
-I_out_expectation_value_as_a_function_of_time_lst = []
-
-for time in time_lst:
-
-        t_load = np.load("t_"+str(np.around(time,2))+".npy",allow_pickle=True)
-        
-        I_in_time = 0.0
-        I_out_time = 0.0
-
-        for i in range(len(t_load)):
-                I_in_time += I_in_coefficients[i]*t_load[i]
-                I_out_time += I_out_coefficients[i]*t_load[i]
-
-        I_in_expectation_value_as_a_function_of_time_lst.append(I_in_time)
-        I_out_expectation_value_as_a_function_of_time_lst.append(I_out_time) 
-
-I_in_expectation_value_as_a_function_of_time_lst = gamma_in*np.array(I_in_expectation_value_as_a_function_of_time_lst)     
-I_out_expectation_value_as_a_function_of_time_lst = gamma_out*np.array(I_out_expectation_value_as_a_function_of_time_lst)  
-
-# %%
-
-# %% [markdown]
-# ## Current measurement from density matrix measurement
-
-# %%
-time_step_for_trotterization = 0.1
-initial_state_of_system = "0100"
-time_lst = np.linspace(0.0,10,5)
-counts_lst = []
-density_matrices_lst = []
-
-#sim_thermal = Aer.get_backend("qasm_simulator")
-sim_thermal = AerSimulator(noise_model=noise_thermal)
-
-basis_set_1 = ["rz","cx","h"]
-basis_set_2 = ["ecr","id","rz","sx","x"]
-
-I_in_expectation_value = []
-density_matrices_lst = []
-
-for time in time_lst:
-
-        # contstructs the time evolved density matrix quantum circuit
-        h_2_molecule_circuit = time_evolved_density_matrix(time_step_for_trotterization,time,initial_state_of_system)
-                
-        # transpile the circuit
-        circ_tthermal = transpile(h_2_molecule_circuit, sim_thermal, basis_gates = basis_set_2 ,optimization_level = 2)
-        
-        """
-        Qiskit counts the qubits from left to right while calculating the density matrix as opposed to its
-        normal convention of counting from right to left. So we reverse the qubit order.
-        """
-        
-        density_matrix = DensityMatrix.from_instruction(circ_tthermal).reverse_qargs()
-
-        density_matrices_lst.append(density_matrix)
-
-# %% [markdown]
-# #### Current in and out plot
-
-# %%
-# sigma_- acting on the first qubit
-L_in  = kron(I2,kron(I2,kron(I2,(sigma_x - 1j*sigma_y)/2)))
-L_out = kron(I2,kron(I2,kron(I2,(sigma_x + 1j*sigma_y)/2)))
-
-# energy currents definition
-def I_in(hamiltonian, density_matrix):
-        return gamma_in*np.trace(( (L_in.conj().T)@hamiltonian@L_in
-                - (1/2)*hamiltonian@(L_in.conj().T)@L_in
-                - (1/2)*(L_in.conj().T)@L_in@hamiltonian )@density_matrix)
-
-def I_out(hamiltonian, density_matrix):
-        return gamma_out*np.trace(( (L_out.conj().T)@hamiltonian@L_out
-                - (1/2)*hamiltonian@(L_out.conj().T)@L_out
-                - (1/2)*(L_out.conj().T)@L_out@hamiltonian )@density_matrix)
-
-# %%
-from qiskit.quantum_info import partial_trace
-reduced_density_matrix_lst = []
-for rho in density_matrices_lst:
-        # tracing out the ancilla qubit which is q_0
-        rho_reduced = partial_trace(rho, [0])
-        reduced_density_matrix_lst.append(rho_reduced)
-
-# %%
-I_in_lst = []
-I_out_lst = []
-for rho in reduced_density_matrix_lst:
-        I_in_lst.append(I_in(full_hamiltonian,rho).real)
-        I_out_lst.append(I_out(full_hamiltonian,rho).real)
-
-np.save("I_in_lst_from_density_matrix.npy", I_in_lst)
-np.save("I_out_lst_from_density_matrix.npy", I_out_lst)
-
-
-# %%
-"""# System Specification
-n_qubits = 2
-circ = QuantumCircuit(n_qubits)
-circ.initialize([1,0],0)
-# Test Circuit
-circ.h(0)
-for qubit in range(n_qubits - 1):
-    circ.cx(qubit, qubit + 1)
-circ.measure_all()
-#circ.draw("mpl",style="iqp",scale=2)
-
-#sim_thermal = AerSimulator(noise_model=noise_thermal)
-sim_thermal = Aer.get_backend("qasm_simulator")
-
-# Transpile circuit for noisy basis gates
-circ_tthermal = transpile(circ, sim_thermal,basis_gates = ["ecr","id","rz","sx","x"])
-
-# Run and get counts
-result_thermal = sim_thermal.run(circ_tthermal, shots = 8192).result()
-counts_thermal = result_thermal.get_counts()
-statevector = counts_to_statevector(counts_thermal)
-print("Statevector = ",statevector)
-print("\n")
-density_matrix = np.outer(statevector, np.conj(statevector))
-print("Density matrix = ",density_matrix)""";
-
-# %%
-"""# System Specification
-n_qubits = 2
-circ = QuantumCircuit(n_qubits)
-circ.initialize([1,0],0)
-# Test Circuit
-circ.h(0)
-for qubit in range(n_qubits - 1):
-    circ.cx(qubit, qubit + 1)
-#sim_thermal = AerSimulator(noise_model=noise_thermal)    
-sim_thermal = Aer.get_backend("qasm_simulator")
-circ_tthermal = transpile(circ, sim_thermal,basis_gates = ["ecr","id","rz","sx","x"],optimization_level=2)
-density_matrix = DensityMatrix.from_instruction(circ_tthermal)
-print(density_matrix)
-circ.draw("mpl",style="iqp",scale=2)""";
-
-# %%
-"""from qiskit.tools.visualization import plot_histogram
-# Run the noisy simulation
-sim_thermal = AerSimulator(noise_model=noise_thermal)
-#sim_thermal = Aer.get_backend("qasm_simulator")
-
-# Transpile circuit for noisy basis gates
-circ_tthermal = transpile(circ, sim_thermal,basis_gates = ["ecr","id","rz","sx","x"])
-
-# Run and get counts
-result_thermal = sim_thermal.run(circ_tthermal).result()
-counts_thermal = result_thermal.get_counts()
-
-# Plot noisy output
-plot_histogram(counts_thermal)""";
-
-
